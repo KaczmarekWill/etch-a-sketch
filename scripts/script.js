71d9e194e739220ebff7aaa368455fbea6bbf15c
Will Kaczmarek <kaczmarekwill@gmail.com>
2017-10-30 00:31:01 -0400

    Added buttons to clear the grid and change the number of squares

--- scripts/script.js
+++ scripts/script.js
@@ -12,10 +12,32 @@ function generateGrid(gridDimen) {
   $('.gridSquare').css({'height': squareSize+'px', 'width': squareSize+'px'});
 }
 
+function resizeGrid() {
+  var newSize = prompt("Enter a grid size between 1 and 100");
+  if (isNaN(newSize)) {
+    alert("Please enter a whole number between 1 and 100");
+  } else if (newSize < 1) {
+    gridSize = 1;
+  } else if (newSize > 100) {
+    gridSize = 100;
+  } else {
+    gridSize = Math.floor(newSize);
+  }
+  generateGrid(gridSize);
+}
+
 $(document).ready(function() {
   generateGrid(gridSize);
+  
+  $('#clearButton').on('mouseup', function() {
+    generateGrid(gridSize);
+  });
 
-  $('.gridSquare').on('mouseenter', function() {
+  $('#gridResize').on('mouseup', function() {
+    resizeGrid();
+  });
+
+  $(document).on('mouseenter', '.gridSquare', function() {
     $(this).css('background-color', '#000');
   });
 });
